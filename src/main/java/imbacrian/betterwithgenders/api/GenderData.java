b6bce3cdcd383972e6d737d4f694a020499e6b43
Bacrian <110308879+Bacrian@users.noreply.github.com>
2026-08-01 19:11:00 -0400

    OH MY GOD, INTELLIJ STOP MESSING ME AROUND. FUCK YOU

---
+++ src/main/java/imbacrian/betterwithgenders/api/GenderData.java
@@ -0,0 +1,39 @@
+package imbacrian.betterwithgenders.api;
+
+public interface GenderData {
+	Gender bwg$getGender();
+	void bwg$setGender(Gender gender);
+
+	float bwg$getBreastSize();
+	void bwg$setBreastSize(float size);
+
+	boolean bwg$isJiggleEnabled();
+	void bwg$setJiggleEnabled(boolean value);
+
+	float bwg$getJiggleAmount();
+	void bwg$setJiggleAmount(float value);
+
+	float bwg$getJiggleDirection();
+	void bwg$setJiggleDirection(float angle);
+
+	boolean bwg$isJiggleWithArmor();
+	void bwg$setJiggleWithArmor(boolean value);
+
+	double bwg$getJiggleTimer();
+	void bwg$setJiggleTimer(double value);
+
+	long bwg$getJiggleLastRenderTick();
+	void bwg$setJiggleLastRenderTick(long value);
+
+	double bwg$getLastWobble();
+	void bwg$setLastWobble(double value);
+
+	boolean bwg$isWasOnGround();
+	void bwg$setWasOnGround(boolean value);
+
+	double bwg$getPrevFallSpeed();
+	void bwg$setPrevFallSpeed(double value);
+
+	double bwg$getFallImpulse();
+	void bwg$setFallImpulse(double value);
+}
